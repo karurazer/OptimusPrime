662b0429974ae920f8a28096d68f2b73bb6017ff
karurazer <karurazer@gmail.com>
2025-02-24 14:13:59 +0100

    Added ability to control number of rotations.

--- robot_wall/motor_control.h
+++ robot_wall/motor_control.h
@@ -3,15 +3,15 @@
 // PINS
 #define MA1 10 // left 
 #define MA2 11 // left
-#define ENA 00
 
 #define MB1 6 // right
 #define MB2 5 // right
-#define ENB 00
 
 #define SR1 3 // left wheel
 #define SR2 2 // right wheel
 
+#define TRIG 9
+#define ECHO 8
 // motor values
 const int pulses = 20;
 int rotationsSr1 = 0;
@@ -28,6 +28,14 @@ void countRotationsSr1()
 void countRotationsSr2() 
 {
   rotationsSr2++; 
+}
+void printInfo(String info)
+{
+  if (millis() - sensorDataInterval >= timerSensor)
+  {
+    timerSensor = millis();
+    Serial.println(info);
+  }
 }
 void read_sensors() 
 {
@@ -51,97 +59,142 @@ void read_sensors()
 }
 void right_s()
 {
-  digitalWrite(MB1, LOW);
-  digitalWrite(MB2, LOW);
+  digitalWrite(MB1, HIGH);
+  digitalWrite(MB2, HIGH);
 }
 void left_s()
 {
-  digitalWrite(MA1, LOW);
-  digitalWrite(MA2, LOW);
+  digitalWrite(MA1, HIGH);
+  digitalWrite(MA2, HIGH);
 }
 void all_s()
 {
   left_s();
   right_s();
 }
-void left_f()
+void left_f(int speed)
 {
-  digitalWrite(MA1, LOW);
-  digitalWrite(MA2, HIGH);
+  analogWrite(MA2, speed);
+    analogWrite(MA1, 0);
 }
-void right_f()
+void right_f(int speed)
 {
-  digitalWrite(MB1, HIGH);
-  digitalWrite(MB2, LOW);
+  analogWrite(MB1, speed);
+    analogWrite(MB2, 0);
 }
-void right_b()
+void right_b(int speed)
 {
-  digitalWrite(MB1, LOW);
-  digitalWrite(MB2, HIGH);
+  analogWrite(MB2, speed);
+    analogWrite(MB1, 0);
 }
-void left_b()
+void left_b(int speed)
 {
-  digitalWrite(MA1, HIGH);
-  digitalWrite(MA2, LOW);
+  analogWrite(MA1, speed);
+    analogWrite(MA2, 0);
 }
-void forward()
+void forward(int speed)
 {
-  right_f();
-  left_f();
+  right_f(speed);
+  left_f(speed);
 }
-void back()
+void back(int speed)
 {
-  right_b();
-  left_b();
+  right_b(speed);
+  left_b(speed);
 } 
 void setRightMotor(int speed)
 {
   if (speed > 0)
   {
-    right_f();
+    right_f(speed);
   } else if (speed < 0)
   {
-    right_b();
+    right_b(speed * -1);
   } else
   {
     right_s();
   }
-  analogWrite(ENB, speed);
 }
 void setLeftMotor(int speed)
 {
   if (speed > 0)
   {
-    left_f();
+    left_f(speed);
   } else if (speed < 0)
   {
-    left_b();
+    speed = speed * -1;
+    left_b(speed);
   } else
   {
     left_s();
   }
-  analogWrite(ENA, speed);
 }
 void setBothMotor(int speed)
 {
   setLeftMotor(speed);
   setRightMotor(speed);
 }
-// void stop_after(int rotations) 
-// {
-//   int count = 0;
+void stopAfterLeft(float rotations) 
+{
+  rotationsSr1 = 0; 
+  while (rotationsSr1 < (rotations * 20)) {
+    Serial.print("Left wheel rotations: ");
+    Serial.println(rotationsSr2);
+  }
+  left_s(); 
+}
+void stopAfterRight(float rotations) {
+  rotationsSr2 = 0; 
+  while (rotationsSr2 < (rotations * 20)) {
+    Serial.print("Right wheel rotations: ");
+    Serial.println(rotationsSr2);
+  }
+  right_s(); 
+}
 
-//   while (count < rotations) 
-//   { 
-//     if (digitalRead(SR1) == LOW || digitalRead(SR2) == LOW) 
-//     {
-//       count++;
-//       Serial.print("Rotation count: ");
-//       Serial.println(count);
-//       delay(50);  
-//     }
-//   }
-//   all_s();
-// }
 
+
+float getDistance() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  
+  long duration = pulseIn(ECHO, HIGH);
+  float distance = duration * 0.034 / 2; 
+
+  return distance;
+}
+
+bool stopDistance(int threshold) {
+  float distance = getDistance();
+  
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance > 0 && distance <= threshold) {
+    Serial.println("Obstacle detected! Stopping.");
+    return true;
+  }
+  return false;
+}
+bool moreDistance(int threshold) {
+  float distance = getDistance();
+  
+  Serial.print("More than distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance > 0 && distance >= threshold) {
+    Serial.println("Distance is good.");
+    return true;
+  } 
+  return false;
+  
+}
+void turnOnDegrees(int degr){
+  stopAfterRight(1);
+}
 #endif
--- robot_wall/robot_wall.ino
+++ robot_wall/robot_wall.ino
@@ -6,22 +6,35 @@ void setup()
   Serial.begin(9600);
   pinMode(MA1, OUTPUT);
   pinMode(MA2, OUTPUT);
-  pinMode(ENA, OUTPUT);
 
   pinMode(MB1, OUTPUT);
   pinMode(MB2, OUTPUT);
-  pinMode(ENB, OUTPUT);
 
   pinMode(SR1, INPUT);
   pinMode(SR2, INPUT);
 
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
   attachInterrupt(digitalPinToInterrupt(SR1), countRotationsSr1, FALLING);
   attachInterrupt(digitalPinToInterrupt(SR2), countRotationsSr2, FALLING);
+  all_s();
+}
+int speed = 250;
+void optimus(){
+  if (stopDistance(12)){
+    setLeftMotor(-speed);
+    setRightMotor(-speed);
+  } else if(moreDistance(20)){
+    setBothMotor(speed);
+  }
+  
 }
 void loop()
 {
-  read_sensors();
-  setBothMotor(-255);
+  left_f(250);
+  stopAfterLeft(0.75);
+  delay(500);
+
 }
 
 
